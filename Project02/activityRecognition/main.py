71e41bbfbb1014d925246266f52cd888df42481d
ktranfaglia1 <kyletranfaglia@gmail.com>
2024-05-08 20:38:41 -0400

    Finally finsihed Project02

--- Project02/activityRecognition/main.py
+++ Project02/activityRecognition/main.py
@@ -1,7 +1,7 @@
 """
 Kyle Tranfaglia
 COSC311 - Project02
-Last updated 05/07/24
+Last updated 05/08/24
 Task 1: Data Segmentation - segmentation of data with sliding window.
 Task 2: Feature Extraction - Each segment is used to extract multiple features to represent an activity.
 Task 3: Dataset Generation - Combination of all features and corresponding activity labels to generate sample. Features
@@ -15,7 +15,7 @@ import pandas as pd
 import seaborn as sb
 import matplotlib.pyplot as plt
 
-from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_val_score, cross_val_predict
 from sklearn.svm import SVC
 from sklearn.pipeline import make_pipeline
 from sklearn.linear_model import LogisticRegression
@@ -43,7 +43,7 @@ def sliding_window(df, win_len):
 
 # Determine the best window length for segmentation
 def best_window_length(act_data, all_labels):
-    window_lengths = range(64, 1025, 64)  # Define window length range to run test
+    window_lengths = range(32, 1025, 32)  # Define window length range to run test
     best_accuracy = 0
     best_window_length = 0
     # Iterate over each window length in range
@@ -101,13 +101,13 @@ def display_confusion_matrix(cm):
     # Display a heatmap / confusion matrix using matplotlib and the sklearn toolset
     matrix_display = ConfusionMatrixDisplay(confusion_matrix=cm)
     fig, ax = plt.subplots(figsize=(10, 8))  # Create layout and structure figure
-    matrix_display.plot(ax=ax, cmap='Blues')  # Create Plot
+    matrix_display.plot(ax=ax, cmap='Reds')  # Create Plot
     # Plot labels
     plt.xlabel('Predicted Labels')
     plt.ylabel('True Labels')
-    plt.title('Voting Confusion Matrix')
-    plt.savefig('confusion_matrix.png')  # Save plot as png
-    # plt.show()  # Display plot
+    plt.title('Confusion Matrix')
+    # plt.savefig('confusion_matrix.png')  # Save plot as png
+    plt.show()  # Display plot
 
 
 # Evaluate classifiers using self test
@@ -127,17 +127,26 @@ def evaluate_self_test(classifier, features, labels, display=0):
 def evaluate_independent_test(classifier, features_train, labels_train, features_test, labels_test, display=0):
     classifier.fit(features_train, labels_train)  # Fit the classifier on the training data
     predictions = classifier.predict(features_test)  # Make predictions on the test data
-    result = accuracy_score(labels_test, predictions)  # Calculate accuracy
-
-    return result
+    if display == 0:
+        return accuracy_score(labels_test, predictions)  # Calculate accuracy
+    elif display == 1:
+        return classification_report(labels_test, predictions)  # Create classification report
+    else:
+        cm = confusion_matrix(labels_test, predictions)  # Create confusion matrix
+        display_confusion_matrix(cm)
 
 
 # Evaluate classifiers using cross-validation test
 def evaluate_cross_validation(classifier, features, labels, display=0):
     scores = cross_val_score(classifier, features, labels, cv=10)  # Use cross_val_score to perform cross-validation
-    result = np.mean(scores)  # Calculate the average accuracy
-
-    return result
+    predictions = cross_val_predict(classifier, features, labels, cv=6)  # Predict true labels
+    if display == 0:
+        return np.mean(scores)  # Calculate the average accuracy
+    elif display == 1:
+        return classification_report(labels, predictions)  # Create classification report
+    else:
+        cm = confusion_matrix(labels, predictions)  # Create confusion matrix
+        display_confusion_matrix(cm)
 
 
 # Condense features by eliminating them based on importance
@@ -195,15 +204,15 @@ samples = np.array(features_per_segment)
 labels = np.array(labels_per_segment)
 
 # Normalize the features
-scaler = MinMaxScaler()
+scaler = StandardScaler()
 samples_normalized = scaler.fit_transform(samples)
 
 # Define classifiers
 classifiers = {
-    "KNN": KNeighborsClassifier(n_neighbors=1),
+    "SVC": SVC(kernel='linear', C=6, probability=True, gamma='scale', random_state=7),
+    "KNN": KNeighborsClassifier(n_neighbors=3),
     "Random Forest": RandomForestClassifier(n_estimators=315, criterion='gini', max_depth=14, min_samples_split=3,
                                             min_samples_leaf=3, max_features='sqrt', random_state=7),
-    "SVC": SVC(kernel='linear', C=6, probability=True, gamma='scale', random_state=7),
     "MLP": MLPClassifier(hidden_layer_sizes=100, activation='tanh', solver='adam', alpha=1e-5, batch_size=36, tol=1e-6,
                          learning_rate_init=0.01, learning_rate='constant', max_iter=10000, random_state=7),
     "Logistic Regression": LogisticRegression(solver='liblinear', random_state=7),
@@ -211,8 +220,8 @@ classifiers = {
 }
 
 # Add VotingClassifier separately in order to use all classifiers in dict for voting
-# voting_classifier = VotingClassifier(estimators=list(classifiers.items()), voting='soft')
-# classifiers["Voting"] = voting_classifier
+voting_classifier = VotingClassifier(estimators=list(classifiers.items()), voting='soft')
+classifiers["Voting"] = voting_classifier
 
 print("\nNormalization vs No Normalization Testing")
 
@@ -244,7 +253,7 @@ for classifier_name, classifier_obj in classifiers.items():
     independent_test_results = evaluate_independent_test(classifier_obj, x_train, y_train, x_test, y_test)
     cross_validation_results = evaluate_cross_validation(classifier_obj, samples_normalized, labels)
     result_average = (self_test_results + independent_test_results + cross_validation_results) / 3
-    classifiers_results[classifier_name] = result_average
+    classifiers_results[classifier_name] = result_average  # Append to dictionary of average accuracy results
 
     # Compare and display the average performance metrics
     print("Test Classifier:", classifier_name)
@@ -253,10 +262,28 @@ for classifier_name, classifier_obj in classifiers.items():
     print("Average accuracy with cross-validation:", cross_validation_results)
     print("Average accuracy of all tests", result_average)
 
+# Sort the dictionary containing the average accuracy results
 sorted_classifier_results = dict(sorted(classifiers_results.items(), key=lambda item: item[1], reverse=True))
 
+# Display ranking of all classifiers, ordered from highest to lowest average accuracy
 print("\nClassifier Rankings")
 for i, (key, value) in enumerate(sorted_classifier_results.items()):
     print(str(i + 1) + ". ", key + ":", value)
 
-best_classifier = next(iter(sorted_classifier_results))
+best_classifier = next(iter(sorted_classifier_results))  # Get the best classifier (first in sorted dictionary)
+
+# Display classification report for best classifier with self-test, independent-test, and CVT
+# The same functions for accuracy evaluations are used but with an end parameter of 1 to denote classification
+self_test_results = evaluate_self_test(classifiers[best_classifier], samples_normalized, labels, 1)
+independent_test_results = evaluate_independent_test(classifiers[best_classifier], x_train, y_train, x_test, y_test, 1)
+cross_validation_results = evaluate_cross_validation(classifiers[best_classifier], samples_normalized, labels, 1)
+
+print("\nClassification reports for best classifier:", best_classifier)
+print("Classification report with self-test:\n", self_test_results)
+print("Classification report with independent-test:\n", independent_test_results)
+print("Classification report with cross-validation:\n", cross_validation_results)
+
+# Display the confusion matrix in a figure for best classifier with independent-test and CVT
+# The same functions for accuracy evaluations are used but with an end parameter of 2 to denote confusion matrix
+evaluate_independent_test(classifiers[best_classifier], x_train, y_train, x_test, y_test, 2)
+evaluate_cross_validation(classifiers[best_classifier], samples_normalized, labels, 2)
